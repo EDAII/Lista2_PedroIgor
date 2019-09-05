ccc9e8232d3e9e4e46e6142a6c44c2cbcb13edf3
Pedro Igor <pedroigor@aluno.unb.br>
2019-09-05 11:45:01 -0300

    Alterando cores das cartas selecionadas.

--- src/start_game.py
+++ src/start_game.py
@@ -20,7 +20,12 @@ def start():
     # criando superfície das cartas
     card_skin = pygame.Surface((60, 90))
     card_skin.fill((150, 65, 200))
-
+    card_skin_first = pygame.Surface((60, 90))
+    card_skin_first.fill((0,0,255))
+    card_skin_second = pygame.Surface((60, 90))
+    card_skin_second.fill((0,255,0))
+    first = []
+    second = []
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -28,11 +33,25 @@ def start():
                 exit()
             elif event.type == MOUSEBUTTONDOWN:
                 mouse_position = (pygame.mouse.get_pos()[1], pygame.mouse.get_pos()[0])
-                print(mouse_position)
                 if mouse_position[0] >= 200 and mouse_position[0] <= 290:
-                    print(ceil(mouse_position[1]/70))
+                    card_selected = ceil(mouse_position[1]/70)-1
+                    if first == []:
+                        first = card_selected
+                    elif second == []:
+                        second = card_selected
+                    else:
+                        first = card_selected
+                        second = []
+
         pygame.display.update()
         screen.fill((0, 0, 0))
-        for i in card_pos:
-            screen.blit(card_skin, i)
+        i = 0
+        while i < len(card_pos):
+            show = card_skin
+            if i == first:
+                show = card_skin_first
+            elif i == second:
+                show = card_skin_second
+            screen.blit(show, card_pos[i])
+            i+=1
 
